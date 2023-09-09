978992f10ceb3f8efd39a59a803f883e6cde0f32
rafaelsalass <rafaelsalass20@gmail.com>
2023-09-08 19:29:55 -0500

    change the default web page title

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hola me llamo edimer</h1>
+      <h2>el segundo titulo</h2>
+      <h3>el segundo titulo</h3>
+      <h4>el segundo titulo</h4>
+      <h5>el segundo titulo</h5>
+      <h6>el segundo titulo</h6>
     </>
   )
 }
